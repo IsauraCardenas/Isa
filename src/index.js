af8e009c4395ec3d3aef67c6e191daa6d2838611
IsauraCardenas <isauracardenas9@gmail.com>
2024-05-16 13:09:41 -0400

    Added basic search engine

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-let searchFormElement = document.querySelector("search-form");
-searchFormElement.addEventListener("submit");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
